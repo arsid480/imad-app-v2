784d1843e8fac0cbaa2533c0863da3a9d8c8f607
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-02-08 18:13:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1', function(req,res) {
-    res.send('this is the first article');
+     res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
     
 });
 app.get('/article2', function(req,res) {

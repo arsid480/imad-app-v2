e099b25bee80a955253f731863e4733a92d3b151
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-02-22 02:18:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ function moveRight(){
     marginleft=marginleft+10;
     image.style.marginLeft=marginLeft+'px';
 }
-madi.onclick=function(){
+image.onclick=function(){
  var interval=setInterval(moveRight,100);
 };
 

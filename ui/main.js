d35c1b54c64041e69675f30e239ee8fd2c1b0395
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-02-22 02:03:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 alert("hello i m the javascript");
 var element =document.getElementById("main_heading");
-element.innerHtml="this is a client side javascript example so go through it"
+element.innerHTML="this is a client side javascript example so go through it"

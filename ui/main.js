4b69fc93523868e9d41bbdaba8dd1d5c67317ad0
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-02-22 02:10:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element =document.getElementById('main_heading');
 element.innerHTML="this is a client side javascript example so go through it"
 
 var image=document.getElementById('madi');
-image.onclick=function(){
-    image.style.marginLeft='200px';
+madi.onclick=function(){
+   madi.style.marginLeft='200px';
 };
 

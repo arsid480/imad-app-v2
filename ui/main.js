a11917b36cfb62af0e9991f29588a0dd5a75f02e
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-02-22 22:53:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,24 @@
-console.log('Loaded!');
-
-var element =document.getElementById('main_heading');
-
-
-var image=document.getElementById('madi');
-
-var marginLeft;
-marginLeft=0;
-function moveRight(){
-    marginleft=marginleft + 1;
-    image.style.marginLeft=marginLeft + 'px';
-}
-image.onclick=function(){
-    alert("dont click on me")
- var interval=setInterval(moveRight,50);
+var button=document.getElementById('counter');
+button.onclick=function(){
+    //Make a request to the counter endpoint
+     var request=new XMLHttpRequest();
+     
+     //capture the response and store in  a variable
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLRequest.DONE){
+            //take some action
+            if(request.status===200){
+                var counter =request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+                
+            }
+        }
+        //not yet done
+    };
+    
+  //make the request
+  request.open('GET','http://arsid480.imad.hasura-app.io/counter',true);
+  request.send(null);
+   
 };
-

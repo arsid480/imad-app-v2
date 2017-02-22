621f2b992221491d971b0030486ec796cb5dd0c6
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-02-23 00:34:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,10 +22,36 @@ button.onclick=function(){
   request.send(null);
    
 };
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
+    //Make a request to the counter endpoint
+     var request=new XMLHttpRequest();
+     
+     //capture the response and store in  a variable
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLRequest.DONE){
+            //take some action
+            if(request.status===200){
+               var names=request.responseText;
+               names=JSON.parse(name);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+                
+                
+            }
+        }
+        //Not yet done
+    };
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+  //make the request
+  request.open('GET','http://arsid480.imad.hasura-app.io/submit-name?name='+ name,true);
+  request.send(null);
     //make the request to the server and send the name
     
     //capture the list of name and render as a list

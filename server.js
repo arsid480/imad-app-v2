54f7aead615039914be21bdfbdb96027111f65af
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-02-22 22:28:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,11 +89,6 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-var counter=0;
-app.get('/counter', function (req, res){
-   counter=counter+1;
-   res.send(counter.toString());
-});
 
 
 

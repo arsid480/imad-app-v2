ce2157058319a7317bd570c2df69b9910bfe2873
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-02-22 23:26:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ submit.onclick=function(){
     for(var i=0;i<names.length;i++){
         list+='<li>'+name[i]+'</li>';
     }
-    var ul=document.getElementById("namelist");
+    var ul=document.getElementById('namelist');
     ul.innerHTML=list;
     
     

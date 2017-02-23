2015f20c8a491052a2e0c4b56c65b1ade08b2259
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-02-23 19:39:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config={
+    user: 'arsid480',
+    database: 'arsid480',
+    host: 'db.imad.hasura.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
+
+var pool=new Pool(config);
+app.get('/test-db',function(req, res){
+   //make a request
+   //return the response to it
+   pool.query('SELECT * FROM test',function(err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           res.send(JSON.stringify(result.rows));
+       }
+  });
+});
 
 var counter=0;
 app.get('/counter', function (req, res){

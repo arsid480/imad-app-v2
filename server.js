b5e8308d6f551b1cc1961d64b382aeb10f565e17
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-03-09 10:09:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/test-db',function(req, res){
 //hashing 
 function hash(input,salt){
     //how do we create a hash
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,10,'sha512');
         return hashed.toString('hex');
 }
 app.get('/hash/:input',function(req,res){

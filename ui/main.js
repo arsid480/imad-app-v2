3fe0acf5e00a95d50fac6d2a8435c1eb700c95e7
arsid480 <mohdarifsiddiqui66@gmail.com>
2017-03-09 12:07:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,11 +47,42 @@ submit.onclick=function(){
         }
         //Not yet done
     };
+    
+  var submit=document.getElementById('submit_button');
+submit.onclick=function(){
+    //Make a request to the counter endpoint
+     var request=new XMLHttpRequest();
+     
+     //capture the response and store in  a variable
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                console.log('user succesfully logged in');
+                alert('user succesfully logged in');
+            }
+            else if(request.status===403){
+                alert('username or password is incorrect');
+                
+            }else if(request.status===500){
+                alert('something went wrong');
+                
+            }
+        }
+    }
+}
+    
+    
     var nameInput=document.getElementById('name');
     var name=nameInput.value;
+    var username=document.getElementById('username'),value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
   //make the request
-  request.open('POST','http://arsid480.imad.hasura-app.io/submit-name?name='+ name,true);
-  request.send(null);
+  request.open('POST','http://arsid480.imad.hasura-app.io/login',true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.Stringify({username:username, password: password}));
     
     
 };
